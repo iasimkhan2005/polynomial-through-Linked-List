48c3dae7056504befe78d2cc4038b473555b4eb2
Administrator <asimali9596a@gmail.com>
2024-11-01 00:51:37 +0500

    Added Remaining Functions

--- polynomial through Linked List/main.cpp
+++ polynomial through Linked List/main.cpp
@@ -39,6 +39,9 @@ private:
 	typedef Node* NODEPTR;
 	NODEPTR head = NULL;
 public:
+	P() {
+		head = NULL;
+	}
 	//-----------------------------------------------------
 	bool check_Duplicate(int coff,int exp) {
 		NODEPTR p = new Node();
@@ -98,8 +101,92 @@ public:
 
 	}
 	//-----------------------------------------------------
-	P operator+(P &R) {
+	P operator+(P& Poly2) {
+		P result;
+		NODEPTR p1 = head;
+		NODEPTR p2 = Poly2.head;
+		 
+		while (p1 != nullptr || p2 != nullptr) {
+			if (p1 == nullptr ) {
+				result.insertTerm(p2->coefficient, p2->exponent);
+				p2 = p2->next;
+			}
+			else if (p2 == nullptr) {
+				result.insertTerm(p1->coefficient, p1->exponent);
+				p1 = p1->next;
+			}
+			else if (p1->exponent > p2->exponent) {
+				result.insertTerm(p1->coefficient, p1->exponent);
+				p1 = p1->next;
+			}
+			else if (p1->exponent < p2->exponent) {
+				result.insertTerm(p2->coefficient, p2->exponent);
+				p2 = p2->next;
+			}
+			else {
+				result.insertTerm(p1->coefficient + p2->coefficient, p1->exponent);
+				p1 = p1->next;
+				p2 = p2->next;
+			}
+		}
+		return result;
+	}
+	P operator-(P& Poly2) {
+		P result;
+		NODEPTR p1 = head;
+		NODEPTR p2 = Poly2.head;
 
+		while (p1 != nullptr || p2 != nullptr) {
+			if (p1 == nullptr) {
+				result.insertTerm(-p2->coefficient, p2->exponent);
+				p2 = p2->next;
+			}
+			else if (p2 == nullptr) {
+				result.insertTerm(p1->coefficient, p1->exponent);
+				p1 = p1->next;
+			}
+			else if (p1->exponent > p2->exponent) {
+				result.insertTerm(p1->coefficient, p1->exponent);
+				p1 = p1->next;
+			}
+			else if (p1->exponent < p2->exponent) {
+				result.insertTerm(-p2->coefficient, p2->exponent);
+				p2 = p2->next;
+			}
+			else {
+				result.insertTerm(p1->coefficient - p2->coefficient, p1->exponent);
+				p1 = p1->next;
+				p2 = p2->next;
+			}
+		}
+		return result;
+	}
+	P operator*(P& Poly2) {
+		P result;
+		for (NODEPTR p1 = head; p1 != nullptr; p1 = p1->next) {
+			P temp;
+			for (NODEPTR p2 = Poly2.head; p2 != nullptr; p2 = p2->next) {
+				int newCoefficient = p1->coefficient * p2->coefficient;
+				int newExponent = p1->exponent + p2->exponent;
+				temp.insertTerm(newCoefficient, newExponent);
+			}
+			result = result + temp;
+		}
+		return result;
+	}
+
+	//-----------------------------------------------------
+	int evaluate() {
+		int x;
+		cout << "Enter the value for x: ";
+		cin >> x;
+		int result = 0;
+		NODEPTR current = head;
+		while (current) {
+			result += current->coefficient * pow(x, current->exponent);
+			current = current->next;
+		}
+		return result;
 	}
 	//-----------------------------------------------------
 	void Display() {
@@ -111,18 +198,19 @@ public:
 			return;
 		}
 		while (current != nullptr) {
-			if (current->exponent  != 0) 
-			{
-			  cout << current->coefficient << "x^" << current->exponent;
-			  current = current->next;
-			if (current->next != nullptr) {
+			if (current->exponent != 0) {
+				  cout << current->coefficient << "x^" << current->exponent;
+			}
+			else {
+				cout << current->coefficient;
+			}
+			current = current->next;
+			if (current != nullptr && current->coefficient > 0) {
 				cout << " + ";
 			}
-			  continue;
+			if (current != nullptr && current->coefficient < 0) {
+				cout << " ";
 			}
-			cout << " + ";
-			cout << current->coefficient;
-			current = current->next;
 		}
 		cout << endl;
 		system("pause");
@@ -139,6 +227,10 @@ int main() {
 		OutputScreen();
 		cout << "1.Insert the Term." << endl;
 		cout << "2.Display the Polynomial in a readible Format." << endl;
+		cout << "3.ADD two Polynomials." << endl;
+		cout << "4.SUBTRACT two Polynomials." << endl;
+		cout << "5.MULTIPLE two Polynomials." << endl;
+		cout << "6.Evaluation." << endl;
 		cout << "0.EXIT" << endl;
 		cout << "Option: ";
 		cin >> option;
@@ -165,8 +257,69 @@ int main() {
 			list[id].Display();
 			cout << endl;
 		}
-
-		
+		else if (option == '3') {
+			system("cls");
+			OutputScreen();
+			cout << "1st polynomial: " << endl;
+			int id1 = Id();
+			cout << "2nd polynomial: " << endl;
+			int id2 = Id();
+			cout << "Result Polynomial id: ";
+			int resultid;
+			cin >> resultid;
+			if (list.find(id1) != list.end() && list.find(id2) != list.end()) {
+				list[resultid] = list[id1] + list[id2];
+			}
+		}
+		else if (option == '4'){
+			system("cls");
+			OutputScreen();
+			cout << "1st polynomial: " << endl;
+			int id1 = Id();
+			cout << "2nd polynomial: " << endl;
+			int id2 = Id();
+			cout << "Result Polynomial id: ";
+			int resultid;
+			cin >> resultid;
+			if (list.find(id1) != list.end() && list.find(id2) != list.end()) {
+				list[resultid] = list[id1] - list[id2];
+			}
+		}
+		else if (option == '5'){
+			system("cls");
+			OutputScreen();
+			cout << "1st polynomial: " << endl;
+			int id1 = Id();
+			cout << "2nd polynomial: " << endl;
+			int id2 = Id();
+			cout << "Result Polynomial id: ";
+			int resultid;
+			cin >> resultid;
+			if (list.find(id1) != list.end() && list.find(id2) != list.end()) {
+				list[resultid] = list[id1] * list[id2];
+			}
+		}
+		else if (option == '6') {
+			system("cls");
+			OutputScreen();
+			cout << "Enter Polynomial id which you want to evaluate." << endl;
+			int id = Id();
+			cout << endl;
+			if (list.find(id) != list.end()) {
+				cout << "Result: " << list[id].evaluate() << endl;
+			}
+			else {
+				cout << "Polynomial with ID " << id << " does not exist." << endl;
+			}
+			system("pause");
+		}
+		else if (option == '0') {
+			exit(1);
+		}
+		else {
+			cout << "Invalid Input!!!" << endl;
+			system("pause");
+		}
 	} while (true);
 
 }
